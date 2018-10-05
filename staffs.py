166eb2386ede5c237998ed2c05e7f4a136ad8268
Hermes <hermes.espinola@gmail.com>
2018-10-05 00:06:16 -0500

    correct some images

--- staffs.py
+++ staffs.py
@@ -8,30 +8,50 @@ ap.add_argument("-i", "--image", required=True,
                 help="Path to the image to be scanned")
 args = vars(ap.parse_args())
 
-src = cv2.imread(args['image'], cv2.IMREAD_GRAYSCALE)
-cv2.bitwise_not(src, src)  # invert grayscale
+gray = cv2.imread(args['image'], cv2.IMREAD_GRAYSCALE)
+_, src = cv2.threshold(gray, 0, 100, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 kernel = np.ones((5, 5), np.uint8)
-src = cv2.dilate(src, kernel, iterations=2)  # dilate so regions are highlighted
+src = cv2.dilate(src, kernel, iterations=5)  # dilate so regions are highlighted
 
 # column sum
-x_distribution = cv2.reduce(src, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32S)[0]
+x_distribution = np.array(cv2.reduce(src, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32S)[0], dtype=float)
+grad_x = np.gradient(x_distribution)
+len_x = len(grad_x)
+gradd_x = [x for x in grad_x]
+max_left, min_left = np.argmax(grad_x[:len_x//2]), np.argmin(grad_x[:len_x//2])
+left = min(max_left, min_left)
+src[:,:left] = grad_x[:left] = 0
+max_right, min_right = np.argmax(grad_x[len_x//2:][::-1]), np.argmin(grad_x[len_x//2:][::-1])
+right = len(grad_x) - max(max_right, min_right)
+src[:,right:] = grad_x[right:] = 0
+
 # row sum
 y_distribution = cv2.reduce(src, 1, cv2.REDUCE_SUM, dtype=cv2.CV_32S)
 y_distribution = np.array(y_distribution).flatten()
+grad_y = np.gradient(y_distribution)
+len_y = len(grad_y)
+gradd_y = [y for y in grad_y]
+max_left, min_left = np.argmax(grad_y[:len_y//2]), np.argmin(grad_y[:len_y//2])
+left = min(max_left, min_left)
+src[:left,:] = grad_y[:left] = 0
+max_right, min_right = np.argmax(grad_y[len_y//2:][::-1]), np.argmin(grad_y[len_y//2:][::-1])
+right = len(grad_y) - min(max_right, min_right)
+src[right:,:] = grad_y[right:] = 0
 
 plt.subplot(2, 2, 1)
 y_pos = np.arange(len(x_distribution))
-plt.bar(y_pos, x_distribution, align='center')
+plt.bar(y_pos, gradd_x, align='center')
 plt.title('horizontal')
 
 plt.subplot(2, 2, 3)
 y_pos = np.arange(len(y_distribution))
-plt.barh(y_pos, y_distribution, align='center')
+plt.barh(y_pos, gradd_y, align='center')
 plt.gca().invert_yaxis()
 plt.title('vertical')
 
 plt.subplot(1, 2, 2)
-plt.imshow(src, cmap='gray', interpolation='nearest')
+res = cv2.bitwise_and(_src, _src, mask=src)
+plt.imshow(res, cmap='gray', interpolation='nearest')
 plt.title('image')
 
 plt.show()
