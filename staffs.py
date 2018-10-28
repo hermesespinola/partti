013efe8899709531b6842f0eeac6e14a4dc15638
carlosmartell97 <carlosmartell97@gmail.com>
2018-10-28 00:34:29 -0500

    now identifying individual notes from horizontal and vertical distributions

--- staffs.py
+++ staffs.py
@@ -27,25 +27,26 @@ for c in blue_contours:
     # calculate moments for each contour
     M = cv2.moments(c)
 
-    # calculate x,y coordinate of center
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
-    cArea = cv2.contourArea(c)
-    if cArea > 1000:
-        found += 1
-        if found > 4:
-            print("ERROR. More than 4 points of reference.")
-            exit(1)
-        src = cv2.circle(src, (cX, cY), 15, (0, 255, 0), 3)
-        if cX < left_most:
-            left_most = cX
-        if cX > right_most:
-            right_most = cX
-        if cY < down_most:
-            down_most = cY
-        if cY > up_most:
-            up_most = cY
-    print "\tx:%d  y:%d area:%d" % (cX, cY, cArea)
+    if M["m00"] > 0:
+        # calculate x,y coordinate of center
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        cArea = cv2.contourArea(c)
+        if cArea > 1000:
+            print "\tx:%d  y:%d area:%d" % (cX, cY, cArea)
+            found += 1
+            if found > 4:
+                print("ERROR. More than 4 points of reference.")
+                exit(1)
+            src = cv2.circle(src, (cX, cY), 15, (0, 255, 0), 3)
+            if cX < left_most:
+                left_most = cX
+            if cX > right_most:
+                right_most = cX
+            if cY < down_most:
+                down_most = cY
+            if cY > up_most:
+                up_most = cY
 if found < 4:
     print("ERROR. Not enough points of reference.")
     exit(1)
@@ -60,14 +61,12 @@ tresh = cv2.dilate(_tresh, kernel, iterations=5)  # dilate to highlight regions
 im, _contours, hierarchy = cv2.findContours(tresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = []
 staff_crops = []
-# _tresh_2 = _tresh.copy()
 for con in _contours:
-    if cv2.contourArea(con) > 15000:
+    if cv2.contourArea(con) > 24000:
         cv2.drawContours(tresh, [con], 0, 255, -1)  # contour filling
         contours.append(con)
         epsilon = 0.01 * cv2.arcLength(con, True)
         approx = cv2.approxPolyDP(con, epsilon, True)
-        # cv2.drawContours(_tresh_2, [approx], -1, (255, 255, 255), 3)
         left_most, up_most, right_most, down_most = _tresh.shape[1], -1, -1, _tresh.shape[0]
         for coord in approx:
             cX=coord[0][0]
@@ -80,7 +79,6 @@ for con in _contours:
                 down_most = cY
             if cY > up_most:
                 up_most = cY
-        # _tresh_2 = cv2.rectangle(_tresh_2, (left_most, up_most), (right_most, down_most), (255, 255, 255), 3)
         staff_crops.append(_tresh[down_most:up_most, left_most:right_most])
 valid_blobs = len(contours)
 print("valid blobs found: %d" % valid_blobs)
@@ -88,19 +86,53 @@ print("valid blobs found: %d" % valid_blobs)
 res = cv2.bitwise_and(gray, gray, mask=tresh)
 res = cv2.drawContours(res, contours, -1, (255, 0, 0), 2)
 
-columns = 4
+columns = 3
 plt.subplot2grid((1, columns), (0, 0)), plt.title('original'), plt.imshow(to_crop[..., ::-1])  # BGR -> RGB
 plt.subplot2grid((1, columns), (0, 1)), plt.title('cropped & segmented'), plt.imshow(res, cmap='gray', interpolation='nearest')
-# plt.subplot2grid((1, columns), (0, 2)), plt.title('binary with approx squares'), plt.imshow(_tresh_2, cmap='gray', interpolation='nearest')
 for i in range(valid_blobs):
-    plt.subplot2grid((valid_blobs, columns), (i, columns-2), rowspan=1), plt.title('staff %d' % (i+1))
-    plt.imshow(staff_crops[valid_blobs-1-i], cmap='gray', interpolation='nearest')
+    staff_crop = staff_crops[valid_blobs-1-i]
 
-    plt.subplot2grid((valid_blobs, columns), (i, columns-1), rowspan=1), plt.title('distribution %d' % (i+1))
-    # column sum
-    x_distribution = cv2.reduce(staff_crops[valid_blobs-1-i], 0, cv2.REDUCE_SUM, dtype=cv2.CV_32S)[0]
-    y_pos = np.arange(len(x_distribution))
-    plt.bar(y_pos, x_distribution, align='center')
+    x_distribution = cv2.reduce(staff_crop, 0, cv2.REDUCE_SUM, dtype=cv2.CV_32S)[0]  # column sum
+
+    plt.subplot2grid((valid_blobs, columns), (i, columns-1), rowspan=1), plt.title('notes %d' % (i+1))
+    min = staff_crop.shape[1] * 255
+    x_1 = -1
+    for val in x_distribution[50:len(x_distribution)-50]:
+        if val < min:
+            min = val
+    min += 2000
+    # print 'notes %d' % (i+1)
+    # print "min: %d" % min
+    staff_crop_binary = staff_crop.copy()
+    staff_crop = cv2.cvtColor(staff_crop, cv2.COLOR_GRAY2RGB)
+    for j in range(len(x_distribution)):
+        val = x_distribution[j]
+        if val < min and x_1 != -1 and j-x_1 > 5:
+            # print "ENDS from %d to %d: %d" % (x_1, j, val)
+            longest_consecutive = 0
+            count = 0
+            y_start = 0
+            y_end = staff_crop.shape[0]
+            note_horizontal_crop = staff_crop_binary[0:staff_crop.shape[0], x_1:j]
+            y_distribution = np.array(cv2.reduce(note_horizontal_crop, 1, cv2.REDUCE_SUM, dtype=cv2.CV_32S).flatten())  # row sum
+            for k in range(len(y_distribution)):
+                row_sum = y_distribution[k]
+                if row_sum > 0:
+                    if count == 0:
+                        y_start_temp = k
+                    count += 1
+                else:
+                    if count > longest_consecutive:
+                        longest_consecutive = count
+                        y_start = y_start_temp
+                        y_end = k
+                    count = 0
+            staff_crop = cv2.rectangle(staff_crop, (x_1, y_start), (j, y_end), (255, 0, 0), 1)
+            x_1 = -1
+        elif val > min and x_1 == -1:
+            # print "STARTS at %d: %d" % (j, val)
+            x_1 = j
+    plt.imshow(staff_crop)
 plt.show()
 
 cv2.waitKey()
